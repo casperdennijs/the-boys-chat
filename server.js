c3d3e5a1c567ded85b76c84c84ab53c1142d3d07
Casper <casper.dennijs@outlook.com>
2023-04-17 11:17:14 +0200

    Port for Adaptable

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,7 +14,7 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
 
